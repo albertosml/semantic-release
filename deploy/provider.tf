c8e0fadd11789e84c285a475cfcba95d8aab3f18
Alberto Silvestre Montes Linares <albertosilvestremonteslinares@gmail.com>
2025-02-20 23:51:59 +0100

    Provider

--- deploy/provider.tf
+++ deploy/provider.tf
@@ -14,7 +14,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "book-recommender"
-    storage_account_name = "book-recommender-storage-account"
+    storage_account_name = "book-recommender-stoacc"
     container_name       = "tfstate"
     key                  = "book-recommender.tfstate"
   }
